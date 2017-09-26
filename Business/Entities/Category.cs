4435498eff72a9e172b771ca8ae29e0bb287f45a
creis <cristiano@prion.com.br>
2017-09-26 13:19:21 -0300

    Criação das entidades

--- Business/Entities/Category.cs
+++ Business/Entities/Category.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Business
+namespace Business.Entities
 {
-    public class Class1
+    public class Category
     {
     }
 }
