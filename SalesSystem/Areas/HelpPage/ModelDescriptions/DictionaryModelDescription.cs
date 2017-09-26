ab53284ced805f5baf0e8bf136f2b7601ad673ef
creis <cristiano@prion.com.br>
2017-09-26 13:08:29 -0300

    Criação da estrutura do projeto iniciada

---
+++ SalesSystem/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SalesSystem.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SalesSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SalesSystem.WebApiApplication" Language="C#" %>
